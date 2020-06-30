61bcb84973ed2529f40ae2a533878e780dec53d5
Stefan Richter <sealor@users.noreply.github.com>
2020-06-30 22:14:17 +0200

    Add initial configuration

---
+++ init.el
@@ -0,0 +1,19 @@
+(package-initialize)
+(find-file (concat user-emacs-directory "config.org"))
+(org-babel-tangle)
+(load-file (concat user-emacs-directory "config.el"))
+
+;; START OF AUTO-GENERATED CONTENT
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
